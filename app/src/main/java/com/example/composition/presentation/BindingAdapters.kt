0521a9afcdc6bfee9583e7da1c136383960ab0e4
Vladyslav Horban <ywvvvw@gmail.com>
2023-07-22 17:39:29 +0300

    Add data binding to GameFragment

--- app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/composition/presentation/BindingAdapters.kt
@@ -1,10 +1,18 @@
 package com.example.composition.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.composition.R
 import com.example.composition.domain.entity.GameResult
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswer(textView: TextView, count: Int) {
@@ -57,4 +65,41 @@ private fun getSmileResId(winner: Boolean): Int {
     } else {
         R.drawable.ic_sad
     }
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, enough))
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColorByState(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+private fun getColorByState(context: Context, goodState: Boolean): Int {
+    val colorResId = if (goodState) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString().toInt())
+    }
+}
+
+@BindingAdapter("progress")
+fun bindProgress(progressBar: ProgressBar, count: Int) {
+    progressBar.setProgress(count, true)
 }
